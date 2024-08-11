5422c92a6039c1aff70aca453a1f5235e02e45a8
sobraldearruda <rafael.sobral@ccc.ufcg.edu.br>
2024-08-11 14:45:47 -0300

    update enigmas and add utils

--- haskell/Menu.hs
+++ haskell/Menu.hs
@@ -1,18 +1,21 @@
-module Menu (printMenu, chooseOption, toContinue, backToMenu) where
+module Menu (printMenu, chooseOption, backToMenu) where
 
+import Utils (toContinue, printTextScreen)
+import Enigmas (printEnigma)
 import System.Exit (exitSuccess)
 
--- EXIBE O MENU
+-- Exibe o Menu de opções
 printMenu :: IO ()
 printMenu = do
-    putStrLn "\n###### COMPUTING MAZE ######"
-    putStrLn "1 - INICIAR O JOGO"
-    putStrLn "2 - TUTORIAL"
-    putStrLn "3 - SAIR DO JOGO"
+    printTextScreen 
+        ["###### COMPUTING MAZE ######",
+        "1 - INICIAR O JOGO", 
+        "2 - TUTORIAL", 
+        "3 - SAIR DO JOGO"]
     option <- readLn :: IO Int
     chooseOption option
 
--- SELECIONA A OPÇÃO DO USUÁRIO
+-- Seleciona a opção do usuário
 chooseOption :: Int -> IO ()
 chooseOption 1 = startGame
 chooseOption 2 = openTutorial
@@ -23,48 +26,43 @@ chooseOption invalidOption = do
   putStrLn "\n### Opção inválida. Tente novamente. ###"
   printMenu
 
--- CONTINUA O FLUXO DE EXECUÇÃO
-toContinue :: IO ()
-toContinue = do
-    putStrLn "\n### Pressione ENTER para continuar. ###"
-    line <- getLine :: IO String
-    putStrLn "\n"
-
--- RETORNA AO MENU DE OPÇÕES
+-- Retorna ao Menu de opções
 backToMenu :: IO ()
 backToMenu = do
     putStrLn "\n### Pressione ENTER para voltar ao MENU PRINCIPAL. ###"
     line <- getLine :: IO String
     printMenu
 
--- INICIA O JOGO
+-- Inicia o Jogo
 startGame :: IO ()
 startGame = do
     putStrLn "\n### Escrever algo aqui. ###" -- EDITAR MENSAGEM POSTERIORMENTE
     toContinue
 
--- ABRE O TUTORIAL
+-- Abre o Tutorial
 openTutorial :: IO ()
 openTutorial = do
     -- EDITAR MENSAGENS POSTERIORMENTE
-    putStrLn "\nComputing Maze é um jogo 2D de simulação de labirintos "
-    putStrLn "em um contexto do curso de Ciência da Computação."
-    putStrLn "Os labirintos são gerados automaticamente de forma aleatória."
-    putStrLn "A cada fase nova, você vai encontrar um novo labirinto e"
-    putStrLn "um enigma relacionado a um período específico do fluxograma do curso."
-    putStrLn "Seja cauteloso, o seu objetivo final é chegar a conclusão do curso"
-    putStrLn "e conseguir sair desses labirintos com sucesso."
+    printTextScreen 
+        ["Computing Maze é um jogo 2D de simulação de labirintos ",
+        "em um contexto do curso de Ciência da Computação. ",
+        "Os labirintos são gerados automaticamente de forma aleatória. ",
+        "A cada fase nova, você vai encontrar um novo labirinto e ",
+        "um enigma relacionado a um período específico do fluxograma do curso. ",
+        "Seja cauteloso, o seu objetivo final é chegar a conclusão do curso ",
+        "e conseguir sair desses labirintos com sucesso."]
     toContinue
     -- EDITAR MENSAGENS POSTERIORMENTE
-    putStrLn "\nOs enigmas que você vai desvendar são a chave"
-    putStrLn "para conseguir sair definitivamente de cada labirinto."
-    putStrLn "Mas será que você consegue descobrir os mistérios de FMCC?"
-    putStrLn "Ou será que você vai conseguir solucionar a temida"
-    putStrLn "Teoria da Computação? Você acha mesmo ser capaz de"
-    putStrLn "solucionar os quebra-cabeças de ATAL e Compiladores?"
-    putStrLn "Vamos descobrir até onde você consegue chegar nessa jornada."
+    printTextScreen 
+        ["Os enigmas que você vai desvendar são a chave ",
+        "para conseguir sair definitivamente de cada labirinto. ",
+        "Mas será que você consegue descobrir os mistérios de FMCC? ",
+        "Ou será que você vai conseguir solucionar a temida ",
+        "Teoria da Computação? Você acha mesmo ser capaz de ",
+        "desconstruir os quebra-cabeças de ATAL e Compiladores? ",
+        "Vamos descobrir até onde você consegue chegar nessa jornada."]
     toContinue
     -- EDITAR MENSAGEM POSTERIORMENTE
-    putStrLn "\n ESCREVER INSTRUÇÕES AQUI. "
-    
+    printTextScreen 
+        ["ESCREVER INSTRUÇÕES AQUI."]
     backToMenu
