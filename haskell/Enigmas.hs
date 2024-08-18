9f464d690861b6c28b835a0916517de058f32feb
sobraldearruda <rafael.sobral@ccc.ufcg.edu.br>
2024-08-17 21:25:22 -0300

    finish enigmas

--- haskell/Enigmas.hs
+++ haskell/Enigmas.hs
@@ -68,9 +68,46 @@ projetoSoftware = printEnigma
     "api" -- API: Application Programming Interface
 
 -- Enigma de "Análise e Técnicas de Algoritmos"
+analiseAlgoritmos :: IO ()
+analiseAlgoritmos = printEnigma
+    "Eu sou o enigma de Análise e Técnicas de Algoritmos"
+    ["Parece que você está indo muito bem, mas duvido que você consiga passar por mim. ",
+    "O meu enigma vai te fazer pensar se realmente vale a pena seguir em frente: ",
+    "Se você não sabe aonde ir, mas pode verificar todos os caminhos possíveis, ",
+    "na pior das hipóteses, então, vai precisar voltar ao início de sua jornada, ",
+    "ou até verificar com antecedência qual caminho é válido."]
+    "backtracking"
 
 -- Enigma de "Compiladores"
+compiladores :: IO ()
+compiladores = printEnigma
+    "Eu sou o enigma de Compiladores"
+    ["Se você chegou até aqui, então é um ótimo jogador. ",
+    "Mas acho muito difícil você conseguir solucionar o meu enigma: ",
+    "Seu nome é uma estrutura de dados usada para análise, verificação e validação, ",
+    "então se você é uma lista de tuplas que formam uma matriz de coordenadas, ",
+    "sua representação pode ser um labirinto ou o seu próprio nome."]
+    "árvore sintática"
 
 -- Enigma de "Projeto de Computação"
+projetoComputacao :: IO ()
+projetoComputacao = printEnigma
+    "Eu sou o enigma de Projeto de Computação"
+    ["Eu duvidei que você conseguiria chegar até aqui, mas tenho certeza que ",
+    "não vai conseguir resolver o meu enigma: ",
+    "Eu sou um design de Projeto de Software e sigo uma política de cache de memória ",
+    "na qual você é o primeiro a chegar e o primeiro a sair."]
+    "fifo" -- FIFO: First-in, First-out
 
 -- Enigma de "Trabalho de Conclusão de Curso"
+tcc :: IO ()
+tcc = printEnigma
+    "Eu sou o enigma de Trabalho de Conclusão de Curso"
+    ["Enfim, você conseguiu chegar ao último enigma dessa jornada. ",
+    "Para muitos, eu sou o mais temido, mas confesso que tenho tudo a ver ",
+    "com todos os outros enigmas que você já solucionou. Então, essa é a sua ",
+    "chance de provar pela última vez que merece sair do labirinto e chegar a ",
+    "tão sonhada conclusão do curso de Ciência da Computação. ",
+    "Uma dica: a resposta correta para o meu enigma é essencial em todo tipo de pesquisa: ",
+    "Eu sou o meio, diante do início, com vistas a um fim."]
+    "metodologia"
